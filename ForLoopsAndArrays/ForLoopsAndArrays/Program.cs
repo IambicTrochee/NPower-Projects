9d6f26eac65225915ea1fed86dcc92aaab66ab71
IambicTrochee <sally.luong@gmail.com>
2017-03-07 11:27:31 -0500

    Added projects demonstrating iterating through arrays with for loops

---
+++ ForLoopsAndArrays/ForLoopsAndArrays/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoopsAndArrays
+{
+    class ForLoopsAndArraysDemo
+    {
+        static void Main(string[] args)
+        {
+            //creates an array
+            int[] array = new int[] { 1, 2, 3, 4 };
+
+            Console.WriteLine("Iterating through a single-dimensional array using a for loop:");
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+
+            //creates a 3x3 multidimensional array
+            int[,] anArray = new int[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+            //using nested for loops to iterate through the array
+            //notice that j will iterate from 0-2 for every iteration of i
+            Console.WriteLine("\n\nIterating through a multidimensional array using nested for loops:");
+            for (int i = 0; i < 3; i++)                   //i is used to refer to which row is being accessed
+            {
+                for (int j = 0; j < 3; j++)               //j is used to refer to which column is being accessed
+                {
+                    Console.Write("{0} ", anArray[i, j]); //writes the value at the index i,j to the console
+                }
+                Console.Write("\n");                      //line break to represent a new row
+            }
+
+            Console.WriteLine("\nIterating through an array using the foreach statement:");
+            //using the foreach statement to iterate through the array
+            //every index is accessed in order
+            foreach (int i in anArray)
+            {
+                System.Console.Write("{0} ", i);          //writes the value at the current index to the console
+            }
+
+            //nested for loops can also be used to access an array backwards
+            Console.WriteLine("\n\nIterating through a multidimensional array backwards with nested for loops:");
+            for (int i = 2; i >= 0; i--)
+            {
+                for (int j = 2; j >= 0; j--)
+                {
+                    Console.WriteLine("{0} is the value at anArray[{1}, {2}].", anArray[i, j], i, j);
+                }
+            }
+
+            //more dimensions? no problem, just nest another for loop inside the others!
+            int[,,] thirdArray = new int[2, 2, 2] { { { 1, 2 }, { 3, 4 } }, { { 5, 6 }, { 7, 8 } } }; //creates a 2x2x2 3D array
+            Console.WriteLine("\nIterating through a three-dimensional array using nested for loops:");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    for (int k = 0; k < 2; k++)
+                    {
+                        Console.WriteLine("{0} is the value at thirdArray[{1}, {2}, {3}].", thirdArray[i, j, k], i, j, k);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TicTacToeChecker/TicTacToeChecker/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creates a 3x3 multidimensional array representing a Tic-Tac-Toe game, change this to check different games
+            String[,] ticTacToe = new String[3, 3] { { "O", "X", "X" }, { "X", "O", "O" }, { "O", "O", "X" } };
+
+            //prints the game to the console
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write("{0} ", ticTacToe[i, j]);
+                }
+                Console.Write("\n");
+            }
+
+            bool win = false;
+
+            //checks rows for a win
+            for (int i = 0; i < 3; i++)
+            {
+                if (ticTacToe[i, 0] == ticTacToe[i, 1] && ticTacToe[i, 0] == ticTacToe[i, 2])
+                {
+                    win = true;
+                }
+            }
+
+            //checks columns for a win
+            for (int j = 0; j < 3; j++)
+            {
+                if (ticTacToe[0, j] == ticTacToe[1, j] && ticTacToe[0, j] == ticTacToe[2, j])
+                {
+                    win = true;
+                }
+            }
+
+            //checks diagonals for a win
+            if (ticTacToe[0, 0] == ticTacToe[1, 1] && ticTacToe[0, 0] == ticTacToe[2, 2])
+            {
+                win = true;
+            }
+            else if (ticTacToe[0, 2] == ticTacToe[1, 1] && ticTacToe[0, 2] == ticTacToe[2, 0])
+            {
+                win = true;
+            }
+
+            //prints win or tie to the console
+            if (win == true)
+            {
+                Console.WriteLine("TicTacToe!");
+            }
+            else
+            {
+                Console.WriteLine("Tie game.");
+            }
+        }
+    }
+}
