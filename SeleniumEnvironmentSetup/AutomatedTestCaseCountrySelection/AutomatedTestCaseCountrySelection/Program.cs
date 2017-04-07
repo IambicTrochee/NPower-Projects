b7d3797d346f01e87dfb960606ee1d79a7992974
IambicTrochee <sally.luong@gmail.com>
2017-04-07 11:29:37 -0400

    Testing with Selenium

---
+++ SeleniumEnvironmentSetup/AutomatedTestCaseCountrySelection/AutomatedTestCaseCountrySelection/Program.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        //Opens URL
+        driver.Navigate().GoToUrl("http://opensource.demo.orangehrmlive.com/");
+
+        //Enters the username
+        IWebElement username = driver.FindElement(By.Name("txtUsername"));
+        username.SendKeys("john.smith");
+
+        //Enters the password
+        IWebElement password = driver.FindElement(By.Name("txtPassword"));
+        password.SendKeys("john.smith");
+
+        //Clicks login button
+        driver.FindElement(By.Id("btnLogin")).Click();
+
+        //Click 'My Info'
+        driver.FindElement(By.CssSelector("#menu_pim_viewMyDetails > b")).Click();
+
+        //Click 'Contact Details'
+        driver.FindElement(By.CssSelector("#sidenav > li:nth-child(2) > a")).Click();
+
+        //Click 'Edit'
+        driver.FindElement(By.Id("btnSave")).Click();
+
+        //Find the 'Country' dropdown menu and set it to the variable dropdownCountry
+        IWebElement dropdownCountry = driver.FindElement(By.Name("contact[country]"));
+
+        //Selects 'United States' from the dropdown menu
+        dropdownCountry.SendKeys("United States");
+
+        //Saves changes
+        driver.FindElement(By.Id("btnSave")).Click();
+
+        Thread.Sleep(3000);
+        
+        driver.Quit();
+    }
+}
---
+++ SeleniumEnvironmentSetup/SetupEnvironment/SetupEnvironment/EntryPoint.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using System;
+
+class EntryPoint
+{
+    static IWebDriver driver = new ChromeDriver();
+    static IAlert alert;
+
+    static void Main()
+    {
+        string url = "http://testing.todorvachev.com/special-elements/alert-box/";
+        
+        driver.Navigate().GoToUrl(url);
+
+        alert = driver.SwitchTo().Alert();
+        Console.WriteLine(alert.Text);
+        alert.Accept();
+
+        driver.Quit();
+        Console.ReadLine();
+    }
+
+    private static void RedMesssage(string v)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(v);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    private static void GreenMessage(string v)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(v);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
